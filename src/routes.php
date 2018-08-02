6c373d298c4cd411e4ded76b0708865174f365f2
Muhammad Aslam Pangestu Idham <aslampangestu@gmail.com>
2018-08-02 19:05:56 +0700

    contoh slim framework

---
+++ src/routes.php
@@ -0,0 +1,31 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+// Routes
+
+$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->get("/viral/", function (Request $request, Response $response){
+    $sql = "SELECT * FROM llx_isinotif";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    return $response->withJson(["status" => "success", "data" => $result], 200);
+});
+
+$app->get("/viral/{id}", function (Request $request, Response $response, $args){
+    $id = $args["id"];
+    $sql = "SELECT * FROM llx_isinotif WHERE rowid=:id";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute([":id" => $id]);
+    $result = $stmt->fetch();
+    return $response->withJson(["status" => "success", "data" => $result], 200);
+});
